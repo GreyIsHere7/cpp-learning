c9e0e952c0d824b785fdcf55becc302f9d9f806b
GreyIsHere7 <soukdiscord@gmail.com>
2026-06-24 00:07:09 +0200

    add morcel

--- Calculatrice/Calculatrice.cpp
+++ Calculatrice/Calculatrice.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>  // Je me suis embarquée dans un de ces truc moi aussi ... 
+#include <cmath>
 
+std::string userInput; // will be in ASCII so i need to convert it to int or d
+double expression(); 
+double morcel();
+double factor(); 
 
-
-std::string userInput; // will be in ASCII so i need to convert it to int or double 
 int i =0;
 
+    double calculer(){
+        double Total;
+        return Total; 
+    }
 
+    double expression(){
+        return 0;
+    }
 
- 
+//Function to calculate the result of a string of digits and operators, ignores spaces too.  
+    double morcel() {
+        double a = factor(); 
+        double c;
+            while (userInput[i]== '/' || userInput[i]== '*' || userInput[i]== '%'){
+                char op = userInput[i]; 
+                i++;
+                double b = factor();
 
-//Function to convert a string of digits into a double value
+                if (op == '*'){
+                    c = a*b;
+                }
+                if (op == '/'){
+                   c = a/b;
+                }
+
+                if (op == '%') {
+                    c = fmod(a,b);
+                }
+            }
+    return c;
+            }
+
+//Function to convert a string of digits into a double value, ignores spaces too. 
     double factor () {
         while (userInput[i]== ' '){
             i++;
         }
-
         double b = 0;
         while (userInput[i] >= '0' && userInput[i] <= '9') {
                 b = b*10 + (userInput[i]-'0');
                 i++;
-       }
+             }
        return b;
-
     }
 
 
