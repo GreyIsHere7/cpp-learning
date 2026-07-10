51f294387b544e971f3364852975b14c1319a6cd
GreyIsHere7 <soukdiscord@gmail.com>
2026-07-10 14:07:13 +0200

    Add exponents, factorial, parentheses and wire main

--- Calculatrice/Calculatrice.cpp
+++ Calculatrice/Calculatrice.cpp
@@ -7,75 +7,98 @@ std::string userInput; // will be in ASCII so i need to convert it to int or d
 double expression(); 
 double morcel();
 double factor(); 
+double puissance();
 
 int i = 0;
 
-    double calculer(){
-        double Total;
-        return Total; 
-    }
-
     double expression(){
         double a = morcel();
-
             while (userInput[i] == '+' || userInput[i] == '-'){
                 char op = userInput [i];
                 i++;
                 double b = morcel();
-
-                if (op == '+'){
-                    a = a+b;
+                
+                switch (op){
+                    case '+' : a = a+b;
+                    break;
+                    case '-' : a= a-b;
+                    break;
                 }
-                 if (op == '-'){
-                    a = a-b;
-                }
-        
             }
+    return a; }
 
-        return a;
-    }
-
-//Function to calculate the result of a string of digits and operators, ignores spaces too.  
+//Function to calculate the result of a string of digits and operators. 
     double morcel() {
-        double a = factor();
+        double a = puissance();
       
             while (userInput[i]== '/' || userInput[i]== '*' || userInput[i]== '%'){
                 char op = userInput[i]; // Op pour operateur.
                 i++;
-                double b = factor();
+                double b = puissance();
 
-                if (op == '*'){
-                    a = a*b;
-                }
-                if (op == '/'){
-                   a = a/b;
-                }
+                switch (op){
 
-                if (op == '%') {
-                    a = fmod(a,b); // % for double 
+                    case '*' : a = a*b;
+                    break;
+                    case '/' : a = a/b;
+                    break;
+                    case '%' : a = fmod(a,b); 
+                    break;
                 }
-            }
-    return a;
-            }
+            }  
+    return a;}
+
+double puissance(){
+    double a = factor(); 
+
+    if (userInput[i] == '^') {
+        i++; 
+        double b = puissance();
+        a = pow(a,b); }
+        return a;
+    }
 
 //Function to convert a string of digits into a double value, ignores spaces too. 
     double factor () {
         while (userInput[i]== ' '){
             i++;
         }
-        double b = 0;
+
+        if (userInput[i]== '(') {
+            i++; 
+           double a = expression();
+            
+                if (userInput[i] == ')'){
+                i++; }
+                else {
+                std:: cout << "mhh ... seems you forgot a ')' somwhere ? Wanna try again ? :D" << std:: endl; 
+                }
+            return a; 
+            }
+
+        double a = 0;
         while (userInput[i] >= '0' && userInput[i] <= '9') {
-                b = b*10 + (userInput[i]-'0');
+                a = a*10 + (userInput[i]-'0');
                 i++;
-             }
-       return b;
+        }
+        if (userInput[i] == '!'){
+            for (int j = (a-1); j >= 1; j--) {
+                a = a*j;
+            }
+        i++;    
+        return a; 
+        }
+       return a;
     }
 
 
 int main(){
 
-    std::cout << "Hello ! I am a tiny calculator living in this terminal :) ! \nGive me a formula and I can help you resolve it :) !" << std::endl; 
-    userInput = "      45"; // testing will be changed to correct value later :)
-    std::cout << userInput;
+    std::cout << "Hello ! I am a tiny calculator living in this terminal :) ! \nGive me a formula and I can help you resolve it :) (I cannot calculate square roots or anything using letters for now, please be patient:) !" << std::endl; 
+    std::getline (std:: cin, userInput); 
+    
+    double Total = expression(); 
+
+    std::cout << "Total : " << Total << std:: endl; 
     return 0;
 }
